0bf481331de73f79c050a144d91c0b05729b1fca
danyal <abbasidani82@gmail.com>
2023-12-29 16:44:07 +0500

    issue resolved

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
 import {  SafeAreaView } from 'react-native'
 import React from 'react'
-import { RootStack } from '@navigation'
+import { RootStack } from './src/navigation';
+// import { RootStack } from '@navigation'
 
 const App = () => {
   return (
--- src/navigation/Drawer.tsx
+++ src/navigation/Drawer.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import LoadImage from 'screens/LoadImage';
+import   LoadImage   from '../screens/LoadImage';
  
 
 const DrawerStack = createDrawerNavigator();
