dbfe0cc834905557fd73b7a9e69e869ef57aa404
danyal <abbasidani82@gmail.com>
2023-12-29 16:52:58 +0500

    adding image library

--- src/screens/LoadImage.tsx
+++ src/screens/LoadImage.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import FastImage from 'react-native-fast-image'
 
 const LoadImage = () => {
   return (
-    <View>
-      <Text>LoadImage</Text>
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+<FastImage
+        style={{ width: 200, height: 200 }}
+        source={{
+            uri: 'https://unsplash.it/400/400?image=1',
+            headers: { Authorization: 'someAuthToken' },
+            priority: FastImage.priority.normal,
+        }}
+        resizeMode={FastImage.resizeMode.contain}
+    />
     </View>
   )
 }
